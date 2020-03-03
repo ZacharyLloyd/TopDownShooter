0c45ec2ae57b3a53f6c6421fe7f15399192b7960
Zachary Lloyd <zaclloyd@uat.edu>
2020-03-03 00:48:07 -0700

    Scripts created

--- TopDownShooter340/Assets/Scripts/Weapons/Weapon.cs
+++ TopDownShooter340/Assets/Scripts/Weapons/Weapon.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shoot : MonoBehaviour
+public class Weapon : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
