c343e05474c0c449ce179b2392fb2e1817803dae
Zachary Lloyd <zaclloyd@uat.edu>
2020-03-03 00:36:38 -0700

    Animations done for enemy.
    Start working on weapons, shooting, respawning after death.

--- TopDownShooter340/Assets/Scripts/GameManager.cs
+++ TopDownShooter340/Assets/Scripts/GameManager.cs
@@ -6,10 +6,7 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    [SerializeField] private readonly PlayerController player;
-    [SerializeField, Range(0f, 100f), Tooltip("Current health")] public int currentHealth;
-    [SerializeField, Range(0f, 100f), Tooltip("Current max health")] private int maxHealth;
-
+    [SerializeField] private List<GameObject> spawnedPlayers;
 
     //Awake runs before all Starts
     private void Awake()
@@ -25,7 +22,6 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        currentHealth = maxHealth;
         #endregion
     }
     // Start is called before the first frame update
---
+++ TopDownShooter340/Assets/Scripts/Player/Stats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    [SerializeField, Range(0f, 100f), Tooltip("Current health")] public int currentHealth;
+    [SerializeField, Range(0f, 100f), Tooltip("Current max health")] private int maxHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+}
